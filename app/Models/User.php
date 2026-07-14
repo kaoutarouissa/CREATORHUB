8465cd9438fa7e7717bdf8a6b929d690c3e86d06
kaoutar <kaoutar@gmail.com>
2026-07-14 17:23:19 +0100

    creer les relations user avec likes et saves model

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function likedRealisations()
+{
+    return $this->belongsToMany(Realisation::class, 'likes');
+}
+
+public function savedRealisations()
+{
+    return $this->belongsToMany(Realisation::class, 'saves');
+}
 }
